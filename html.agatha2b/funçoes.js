d0d73fdd66d7ea8ba551275872129e4154384078
A-gatha <126285027+A-gatha@users.noreply.github.com>
2023-10-30 14:11:13 -0300

    Delete html.agatha2b directory

--- html.agatha2b/funçoes.js
+++
@@ -1,9 +0,0 @@
-function verificarNumero () {
-    let numeroAleatorio = Math.random ()
-    let numerotDigitado = document.getElementById('number').value;
-    alert(inputDigitado)
-
-    if (numeroAleatorio == numerotDigitado) 
-    alert ('Parabéns, você acertou o número!');
-} else { 
-    alert("oh, que pena você errou, tente novamente!")}
